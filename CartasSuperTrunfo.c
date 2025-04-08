678ee66cf4576a690310c299d89313cf7ee12568
Hyken1 <mauriciols1@outlook.com>
2025-04-08 19:22:01 +0000

    Desafio Novato Completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Cadastro de variaveis//
+//Nome da cidade
+//Codigo da cidade  
+//Populacao da cidade
+//Area da cidade
+//PIB da cidade
+//Numero de pontos turisticos//
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int main(){
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char nome[50], codigo[5];
+    float area, PIB;
+    int populacao, pontosTuristicos;
 
-    return 0;
+    printf("Digite o nome da cidade: ");
+    scanf("%s]", nome);
+   
+    printf("Digite o codigo da cidade: ");
+    scanf("%s", codigo);
+    
+    printf("Digite a populacao da cidade: ");
+    scanf("%d", &populacao);
+   
+    printf("Digite a area da cidade: ");
+    scanf("%f", &area);
+   
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB);
+    
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos);
+
+// Linhas em branco para melhorar a visualizacao //
+    printf("\n"); // Adiciona uma linha em branco
+    printf("\n"); // Adiciona uma linha em branco
+// Impressao dos dados em conjunto de forma clara e organizada //
+    printf("Cidade: %s\n", nome);
+    printf("Código: %s\n", codigo);
+    printf("População: %d\n", populacao);
+    printf("Área da cidade: %fkm²\n", area);
+    printf("PIB: R$%f\n", PIB);
+    printf("Pontos turísticos: %d\n", pontosTuristicos);
+    
+    return 0;  
+
 }
